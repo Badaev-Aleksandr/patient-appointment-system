cf365077b4b3cf52b111b708f8af2cd299ea9008
Aleksandr <badaev92aleksandr@gmail.com>
2025-03-29 14:13:21 +0100

    feat(patient): update field structure and constraints

--- Patient-Appointment-System/src/main/java/de/ait/patientappointmentsystem/model/Patient.java
+++ Patient-Appointment-System/src/main/java/de/ait/patientappointmentsystem/model/Patient.java
@@ -6,7 +6,9 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
 import jakarta.persistence.OneToMany;
+import jakarta.persistence.OneToOne;
 import jakarta.persistence.Table;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
@@ -31,16 +33,21 @@ public class Patient {
     @GeneratedValue(strategy = GenerationType.AUTO)
     private Long id;
 
-    @Column(name = "first_name", nullable = false)
-    private String firstName;
-    @Column(name = "last_name", nullable = false)
-    private String lastName;
+    @Column(name = "full_name", nullable = false)
+    private String fullName;
+
     @Column(unique = true, nullable = false, length = 100)
     private String email;
+
     @Column(name = "phone_number", unique = true, nullable = false, length = 20)
     private String phoneNumber;
+
     @Column(name = "date_of_birth", nullable = false)
     private LocalDate dateOfBirth;
+
+    @OneToOne(cascade = CascadeType.ALL, orphanRemoval = true)
+    @JoinColumn(name = "user_id", nullable = false)
+    private User user;
 
     @OneToMany(mappedBy = "patient", cascade = CascadeType.ALL, orphanRemoval = true)
     private Set<Appointment> appointments = new HashSet<>();
