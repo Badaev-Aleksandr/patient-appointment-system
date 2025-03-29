40eb1172340737b250b4ed9be8ce37fa8648b2b1
Aleksandr <badaev92aleksandr@gmail.com>
2025-03-29 12:35:13 +0100

    Created Entity Appointment

---
+++ Patient-Appointment-System/src/main/java/de/ait/patientappointmentsystem/model/Appointment.java
@@ -0,0 +1,39 @@
+package de.ait.patientappointmentsystem.model;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.time.LocalDateTime;
+
+@Entity
+@Table(name = "appointments")
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class Appointment {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+
+    @ManyToOne
+    @JoinColumn(name = "patient_id", nullable = false)
+    private Patient patient;
+
+    @Column(name = "appointment_date_time",nullable = false)
+    private LocalDateTime appointmentDateTime;
+
+}
