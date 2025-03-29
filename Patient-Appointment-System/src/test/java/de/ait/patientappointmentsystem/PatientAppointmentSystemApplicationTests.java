685f3c27b0f3a8edc38ea5b6619ca680473eeb66
Aleksandr <badaev92aleksandr@gmail.com>
2025-03-29 10:40:30 +0100

    Initialize Spring Boot project

---
+++ Patient-Appointment-System/src/test/java/de/ait/patientappointmentsystem/PatientAppointmentSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package de.ait.patientappointmentsystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PatientAppointmentSystemApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
