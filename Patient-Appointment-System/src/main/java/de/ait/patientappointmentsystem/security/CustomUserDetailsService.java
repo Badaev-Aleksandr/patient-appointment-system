a7b18ac7bcfe7ae83a47e1393ca481d00b2d4b8e
Aleksandr <badaev92aleksandr@gmail.com>
2025-03-30 18:31:28 +0200

    Created CustomUserDetails and Service

---
+++ Patient-Appointment-System/src/main/java/de/ait/patientappointmentsystem/security/CustomUserDetailsService.java
@@ -0,0 +1,25 @@
+package de.ait.patientappointmentsystem.security;
+
+import de.ait.patientappointmentsystem.model.User;
+import de.ait.patientappointmentsystem.repositories.UserRepository;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+
+@Slf4j
+public class CustomUserDetailsService implements UserDetailsService {
+
+    private final UserRepository userRepository;
+
+    public CustomUserDetailsService(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+       return userRepository.findByUsername(username)
+               .map(CustomUserDetails::new)
+               .orElseThrow(()-> new UsernameNotFoundException("User not found: " + username));
+    }
+}
